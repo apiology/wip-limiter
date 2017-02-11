7493c7f1df43b184bfececaffdf460e2f6726533
Vince Broz <broz@macmicro.fios-router.home>
2017-02-11 12:55:15 -0500

    Initial checkin

---
+++ README.md
@@ -0,0 +1,36 @@
+# WIP Limiter
+
+This is a userscript you can install into your browser to get a visual
+hint when your Kanban-style WIP limit has been exceeded.
+
+Currently supports:
+
+* [Asana](https://www.asana.com)
+
+To use, change the titles of your sections or projects to include a
+string like '[5]' at the end of the title.  The 5 represents your
+maximum WIP limit; the background colors will now reflect whether you
+are at or beyond the limit you set.
+
+TODO: Add screenshot GIF if possible
+
+## Installing
+
+### Chrome
+
+Download the wip-limiter.user.js file from this Repo.  Go to Settings
+| Extensions.  Drag the wip-limiter.user.js file onto your browser
+window.
+
+To install, double click on wip-limiter.user.js; this should launch
+your browser's userscript loader.
+
+## Contributions
+
+PRs and issues welcome!
+
+I use this in Chrome, but definitely file an issue if you have issues in other browsers!
+
+Documentation for userscripts in Chrome
+is
+[here](https://www.chromium.org/developers/design-documents/user-scripts).
---
+++ wip-limiter.user.js
@@ -0,0 +1,77 @@
+// ==UserScript==
+// @name WIP Limiter
+// @namespace http://github.com/apiology/
+// @description Add Kanban-style visual WIP limit indicators
+// @match https://app.asana.com/*
+// @require http://code.jquery.com/jquery-latest.js
+// ==/UserScript==
+
+console.log('starting script');
+
+class Header {
+  constructor(header) {
+    this.header = header;
+  }
+
+
+  // header.classList.contains("bar-row")
+  title() {
+    // look for child td with class = barRow-contents
+    // const td = header.getElementsByClassName('barRow-contents')
+    const textareas = this.header.getElementsByClassName('task-row-text-input');
+    if (textareas.length !== 1) {
+      return null;
+    }
+
+    return textareas[0].value;
+  }
+
+// const wipLimit = header => {
+//
+// }
+// const isOver = header => {
+//    header
+// }
+}
+
+
+// let header = new Header();
+// console.log(header.title())
+
+const markWholeDocument = () => {
+  console.log('starting to mark whole document');
+  const headers = document.getElementsByClassName('bar-row');
+  console.log(headers);
+  if (headers.length > 0) {
+    const headerElement = headers[0];
+    const header = new Header(headerElement);
+    console.log(`title is ${header.title()}`);
+  } else {
+    console.log('no headers found');
+  }
+};
+
+// var curLength=0;
+// setInterval(function(){
+//   if ($('.column-header').length!=curLength){
+//     curLength=$('.column-header').length;
+//     // do stuff here
+//   }
+// },100);
+
+
+if (document.body) {
+  console.log('already loaded; running now');
+  markWholeDocument();
+  console.log('ran!');
+} else {
+  console.log('registering listener');
+  document.addEventListener('DOMContentLoaded', markWholeDocument, false);
+  console.log('registered listener');
+}
+
+// // const textareas = header.getElementsByClassName('task-row-text-input');
+// // let headerTitle = textareas[0].value;
+// // let wipFinder = /.*\[(\d*)\]:$/;
+// // let results = wipFinder.exec(headerTitle);
+// // wipLimit = parseInt(results[1])
