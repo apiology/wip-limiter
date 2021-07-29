e567cc1491e09ba1ad12fd3bb70ba8729fa63456
Vince Broz <vince@broz.cc>
2021-07-29 11:02:30 -0400

    Delete files replaced in cookiecutter import

--- local-pointer.user.js
+++
@@ -1,9 +0,0 @@
-// ==UserScript==
-// @name WIP Limiter
-// @namespace http://github.com/apiology/
-// @description Add Kanban-style visual WIP limit indicators
-// @match https://app.asana.com/*
-// @require         file:///Users/broz/src/wip-limiter/wip-limiter.user.js
-// @require http://code.jquery.com/jquery-latest.js
-// @grant           GM_xmlhttpRequest
-// ==/UserScript==
