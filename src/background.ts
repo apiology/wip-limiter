178c0c858fb57a72587a5e39f561320058bbf72a
Vince Broz <vince@broz.cc>
2021-11-21 13:52:32 -0500

    Update template

--- src/background.ts
+++ src/background.ts
@@ -1,4 +1,4 @@
-const { doWork } = require('./wip-limiter');
+import { doWork } from './wip-limiter';
 
 // Called when the user clicks on the browser action.
 chrome.browserAction.onClicked.addListener(doWork);
