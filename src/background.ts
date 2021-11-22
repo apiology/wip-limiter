da25dee4cd9b7154eb17193355310ed4d243af01
Vince Broz <vince@broz.cc>
2021-11-22 11:14:07 -0500

    Update template

--- src/background.ts
+++ src/background.ts
@@ -1,3 +1,9 @@
+/**
+ * background module.
+ *
+ * Initialized when browser extension is loaded.
+ */
+
 import { doWork } from './wip-limiter';
 
 // Called when the user clicks on the browser action.
--- src/wip-limiter.ts
+++ src/wip-limiter.ts
@@ -1,3 +1,9 @@
+/**
+ * wip-limiter module.
+ *
+ * Chrome extension to get a visual hint when your Kanban-style WIP limit has been exceeded.
+ */
+
 export const doWork = (tab: chrome.tabs.Tab) => {
   // No tabs or host permissions needed!
   console.log(`Turning ${tab.url} red!`);
