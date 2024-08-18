040f3a06caec65d3a789abe9734ba2c99c4b4a7f
Vince Broz <vince@broz.cc>
2024-08-18 19:29:28 -0400

    Update template

--- src/chrome-extension/chrome-extension-logger.ts
+++ src/chrome-extension/chrome-extension-logger.ts
@@ -1,9 +1,21 @@
 export class ChromeExtensionLogger {
-  log = console.log;
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  log(message?: any, ...optionalParams: any[]): void {
+    console.log('WIP Limiter', message, ...optionalParams);
+  }
 
-  debug = console.debug;
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  debug(message?: any, ...optionalParams: any[]): void {
+    console.debug('WIP Limiter', message, ...optionalParams);
+  }
 
-  warn = console.warn;
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  warn(message?: any, ...optionalParams: any[]): void {
+    console.warn('WIP Limiter', message, ...optionalParams);
+  }
 
-  error = console.error;
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  error(message?: any, ...optionalParams: any[]): void {
+    console.error('WIP Limiter', message, ...optionalParams);
+  }
 }
